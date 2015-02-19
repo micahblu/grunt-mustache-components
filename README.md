dc343eb2115e85bc12ac3eea4d2a0a0796588801
Micah Blu <micahblu@gmail.com>
2015-02-18 20:48:40 -0800

    Fixed expand bug

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 
 # grunt-mustache-components
 
-> Extends mustache templating with partial like components that accept context
-
-##Concept 
-
-Have you ever had the need to reuse a mustache partial in the same page but with different content? Then this task may be useful. It has a similar syntax to that of a partial accept you can pass name=value pair attributes providing context to the template keys inside.
+> Extends mustache ttemplating with handlebars like components
 
 ## Getting Started
 This plugin requires Grunt `~0.4.5`
@@ -15,14 +11,17 @@ If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out th
 ```shell
 npm install grunt-mustache-components --save-dev
 ```
+
 Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
 
 ```js
 grunt.loadNpmTasks('grunt-mustache-components');
 ```
 
-### Usage
-In your project's Gruntfile, add a section named `mustache_components`
+## The "mustache_components" task
+
+### Overview
+In your project's Gruntfile, add a section named `mustache_components` to the data object passed into `grunt.initConfig()`.
 
 ```js
 grunt.initConfig({
@@ -42,58 +41,55 @@ grunt.initConfig({
 });
 ```
 
-#### Template syntax
+### Options
 
-The component syntax is very similar to that of a mustache partial.
+#### options.separator
+Type: `String`
+Default value: `',  '`
 
-_templates/index.mustache_
-```html
-    {{^ recipe name=chickenRecipe.name ingredients=chickenRecipe.ingredients instructions=chickenRecipe.instructions}}
-    <hr>
-    {{^ recipe name=peaSoupRecipe.name ingredients=peaSoupRecipe.ingredients instructions=peaSoupRecipe.instructions}}
-    <hr>
-    {{^ recipe quantity=3 num=45 name="Turkey Burgers" foo=bar ingredients="Premium ground turkey 93/7 (No Jenny O!)" instructions="Add about 2 tablespoons of Extra Virgin olive oil to a large bowl. Add the ground turkey and work into medium sized balls. Gently smash the turkey balls till they're roughly 1/2 in height. Cook on medium high heat, flipping once and enjoy!"}}
-```
+A string value that is used to do something with whatever.
 
-#### Context
+#### options.punctuation
+Type: `String`
+Default value: `'.'`
 
-_templates/data/index.json_
-```json
-{
-  "chickenRecipe": {
-    "name": "Dry Rub Oven Roasted Chicken",
-    "ingredients": "1 Whole chicken, our special dry rub",
-    "instructions": "Rub our special dry rub on the chicken and put in the oven, prehead 400 degrees and cook for 45 minutes"
+A string value that is used to do something else with whatever else.
+
+### Usage Examples
+
+#### Default Options
+In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
+
+```js
+grunt.initConfig({
+  mustache_components: {
+    options: {},
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
   },
-
-  "peaSoupRecipe": {
-    "name": "Slow Cooked Split Pea Soup",
-    "ingredients": "1 Bag of split peas",
-    "instructions": "Put parts water to peas in a large boiling pot and cook medium low for 2 1/2 hours"
-  }
-}
+});
 ```
 
-#### Component File
+#### Custom Options
+In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
 
-_templates/components/recipe.mustache_
-```html
-
-<h2>{{name}}</h2>
-
-<b>Ingredients:</b>
-<p>{{ingredients}}</p>
-
-<b>Instructions:?</b>
-<p>{{instructions}}</p>
-
+```js
+grunt.initConfig({
+  mustache_components: {
+    options: {
+      separator: ': ',
+      punctuation: ' !!!',
+    },
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+});
 ```
-*Note the component name must match the name provided in the component tag
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
- - 0.0.3
- - 0.0.2
- - 0.0.1
+ - 0.1.0
--- tasks/mustache_components.js
+++ tasks/mustache_components.js
@@ -4,91 +4,29 @@
  *
  * Copyright (c) 2014 Micah Blu
  * Licensed under the MIT license.
- * 
- * @version 0.0.3
  */
 
 'use strict';
 
 module.exports = function(grunt) {
 
-  grunt.registerMultiTask('mustache_components', 'Reusable mustache components that can be passed context', function() {
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
 
+  grunt.registerMultiTask('mustache_components', 'Extends mustache templates with Ember like components', function() {
+
+        
     var mustache = require('mustache');
     var extend = require("extend");
 
     var options = this.options(),
         context = options.context,
         contextDir = options.contextDir,
-        contextFile = '',
         ext ='',
         filename,
         contents,
-        dest = this.data.dest,
-        src = this.data.src[0].replace(/\*.+/g, ''),
-        ignoreDirs = [options.componentsDir, options.partialsDir, options.contextDir],
-        ignoreDirRegex = new RegExp(ignoreDirs.join("|").replace(new RegExp(src, "g"), ''));
-
-
-    var srcdirs, omit = '', template, write = true, file;
-    
-    this.files.forEach(function(filepair){
-      srcdirs = filepair.orig.src[0].split('/');
-      srcdirs.forEach(function(dir){
-        if(!/\*/g.test(dir)){
-          omit += dir + '/';
-        }
-      });
-
-      filepair.src.forEach(function(template){
-
-        ext = template.substr(template.lastIndexOf('.'), template.length);
-        contextFile = template.replace(omit, '').replace(ignoreDirRegex, '').replace(ext, '.json').replace(/\//g, '.');
-
-        // check for a context json file, if present, extend the context object with it
-        if(grunt.file.exists(contextDir + contextFile)){
-            context = extend(context, grunt.file.readJSON(contextDir + contextFile));
-        }
-
-        // grab current contents of template to string
-        contents = grunt.file.read(template);
-
-        // Transculde partials
-        contents = partialsTransclusion(contents, ext, options.partialsDir);
-        
-        // Transclude components
-        contents = componentsTransclusion(contents, context, ext, options.componentsDir);
-
-        // Render template content with context
-        contents = mustache.render(contents, context);
-        
-        // All good write new file to dest
-        ignoreDirs.forEach(function(dir){
-          if(template.indexOf(dir) > -1){
-            write  = false;
-            return;
-          } 
-        });
-        
-        if(write){
-          file = template.replace(omit, dest).replace(ext, (options.ext || '.html'));
-          //console.log("wrote " + file);
-          grunt.file.write(file, contents);
-        }
-        
-      });
-      omit = '';
-      write = true;
-    });
-
-    return;
-
-    if(!grunt.file.isDir(dest)){
-      grunt.log.warn('Destination must be a directory');
-      return false;
-    }
-
-    //console.log(grunt.file.expand({cwd: dest}, '*').join(grunt.util.linefeed));
+        src = this.data.src,
+        dest = this.data.dest;
 
     function partialsTransclusion(template, ext, partialsDir){
 
@@ -110,73 +48,94 @@ module.exports = function(grunt) {
       return template;
     }
 
-    function componentsTransclusion(template, context, ext, componentsDir){
+    function componentsTransclusion(template, ext, componentsDir){
 
-      var components,
-          componentName,
-          component,
-          componentName = '',
-          componentContext = {},
-          varTokens = [],
-          strTokens = [],
-          intTokens = [],
-          pair = [],
-          value = '';
+        var components,
+            componentName,
+            component,
+            componentStr,
+            componentContext = {},
+            componentTokens = '',
+            nvpair = [];
 
-      if(!context){
-        return template;
-      }
-      while(components = template.match(/\{\{\^ (.+)\}\}/g)){
+        while(components = template.match(/\{\{\^ (.+?)\}\}/g)){
 
-        for(var i=0, j=components.length; i<j; i++){
+          for(var i=0, j=components.length; i<j; i++){
 
-          componentName = components[i].match(/\{\{\^\s(\w+)/)[1];
+            // temporarily replace empty spaces within attribute value strings with a '&nbsp;'
+            componentStr = components[i].replace(/(['"].+?)\s(.+?['"])/, '$1&nbsp;$2');
 
-          strTokens = components[i].match(/\w+=(['"][^'"]+['"])/g);
-          intTokens = components[i].match(/\w+=([0-9]+)/g);
-          varTokens = components[i].match(/\w+=([^'"0-9\s\}]+)/g);
+            // separate component string by empty spaces into tokens 
+            componentTokens = componentStr.match(/\{\{\^ (.+?)\}\}/)[1].split(" ");
 
-          component = grunt.file.read(componentsDir + componentName + ext);
+            component = grunt.file.read(componentsDir + componentTokens[0]  + ext);
+            
+            componentTokens = componentTokens.splice(1);
+            
+            componentTokens.forEach(function(token){
+              nvpair = token.split("=");
+              // If token is encapsulated with ['"] then it is a string literal, 
+              // other wise map value from passed context
+              if(nvpair[1].match(/['"]/)){
+                nvpair[1] = nvpair[1].replace(/&nbsp;/g, " ");
 
-         // console.log(component);
-          if(strTokens) {
-            strTokens.forEach(function(nv){
-              pair = nv.split('=');
-              componentContext[pair[0]] = pair[1].replace(/['"]/g, '');
-            });
-          }
-
-          if(intTokens) {
-            intTokens.forEach(function(nv){
-              pair = nv.split('=');
-              componentContext[pair[0]] = pair[1];
-            });
-          }
-
-          if(varTokens) {
-            varTokens.forEach(function(nv){
-              pair = nv.split('=');
-              
-              if(pair[1].match(/\./g)){
-                value = context;
-                pair[1].split('.').forEach(function(prop){
-                  value = value[prop];
-                });
+                componentContext[nvpair[0]] = nvpair[1].replace(/['"]/g, "");
               }else{
-                value = context[pair[1]];
+                componentContext[nvpair[0]] = context[nvpair[1]];
               }
-              componentContext[pair[0]] = value;
-            }); 
+            });
+
+            component = mustache.render(component, componentContext);
+
+            template = template.replace(components[i], component);
           }
-          
-          component = mustache.render(component, componentContext);
-          template = template.replace(components[i], component);
         }
+
+        template = mustache.render(template, context);
+
+        return template;
+    }
+
+    this.filesSrc.forEach(function(template){
+
+      var path = '';
+      src.forEach( function(src) {
+        src = src.substr(0, (src.indexOf("*")));
+
+        if (template.match(src)) {
+          path = template.replace(src, "");
+        }
+      });
+
+      ext = template.substr(template.lastIndexOf('.'), template.length);
+      filename = template.substr(template.lastIndexOf('/') + 1, template.length).replace(ext, "");
+      
+      var dataFile = contextDir + path.replace("/", ".").replace("mustache", "json");
+
+      // check for a context json file, if present,
+      // extend the context object with it
+      if(grunt.file.exists(dataFile)){
+        context = extend(context, grunt.file.readJSON(dataFile));
+      }
+      
+      // grab current contents of template to string
+      contents = grunt.file.read(template);
+
+      // Transculde partials
+      contents = partialsTransclusion(contents, ext, options.partialsDir);
+      
+      // Transclude components
+      contents = componentsTransclusion(contents, ext, options.componentsDir);
+
+      // Render template content with context
+      contents = mustache.render(contents, context);
+
+      // All good write new file to dest
+      if(!template.match(options.componentsDir) && !template.match(options.partialsDir)){
+
+        grunt.file.write(dest + path.replace('.mustache', options.ext), contents);
       }
 
-      template = mustache.render(template, context);
-
-      return template;
-    }
+    });
   });
 };
